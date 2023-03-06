d0f6980816240ab4b308bf5d26d24909521f7810
eliot rafat offor <eliot.rafat.offor@gmail.com>
2023-03-06 17:56:55 +0100

    gemfile

--- app/models/playground.rb
+++ app/models/playground.rb
@@ -5,8 +5,9 @@ class Playground < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :name, :address, :description, :latitude, :longitude, presence: true
 
-  validates :name, :address, :description, presence: true
+  scope :upcoming_sessions, -> { joins(:sessions).where("sessions.date > ?", Datetime.now) }
 
   include PgSearch::Model
   pg_search_scope :search_by_address_and_date,
