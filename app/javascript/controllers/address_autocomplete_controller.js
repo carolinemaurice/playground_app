293860790d4256023b9228759776ed1be72ce8a6
Laurie Segalowitch <lausegalo@gmail.com>
2023-03-08 18:06:20 +0100

    partial-autocomplete in progress

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -4,7 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = { apiKey: String }
 
-  static targets = ["address"]
+  static targets = ["address", "autocomplete"]
 
   connect() {
     console.log(this.apiKeyValue)
@@ -14,11 +14,22 @@ export default class extends Controller {
   update(event) {
     // if (this.addressTarget.value.length >= 5) {
     //   const url = `${this.baseUrl}&text=${this.addressTarget.value}`
-    //   fetch(url).then(response => response.json()).then(data => console.log(data.results))
+    //   fetch(url).then(response => response.json()).then(data => this.#addAddressesToDiv(data.results))
     // }
   }
 
-  #clearInputValue() {
-    this.addressTarget.value = ""
+  #addAddressesToDiv(results) {
+
+    // this.autocompleteTarget.innerHTML = ""
+    // if (results.length) {
+    //   this.autocompleteTarget.classList.remove('d-none')
+    //   results.forEach((result) => {
+    //     const el = `<div><p>${result.formatted}</p></div>`
+
+    //     this.autocompleteTarget.insertAdjacentHTML('beforeend', el)
+    //   })
+    // } else {
+    //   this.autocompleteTarget.classList.add('d-none')
+    // }
   }
 }
