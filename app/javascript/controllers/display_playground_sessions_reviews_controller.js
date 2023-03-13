922672a43f7ade8a5c899882a3651e40fbda5026
Caroline MAURICE <caroline.maurice.henry@gmail.com>
2023-03-13 21:32:13 +0100

    save modifs display sessions and reviews

--- app/javascript/controllers/display_playground_sessions_reviews_controller.js
+++ app/javascript/controllers/display_playground_sessions_reviews_controller.js
@@ -2,11 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display-playground-sessions-reviews"
 export default class extends Controller {
-  static targets = ["sessions-list", "reviews-list"]
+  static targets = ["sessionslink", "reviewslink", "sessionslist", "reviewslist"]
 
   connect() {
-    console.log("")
   }
 
+  displaySessions(event) {
+    event.preventDefault()
+    this.sessionslinkTarget.classList.toggle("mute")
+    this.reviewslinkTarget.classList.toggle("mute")
+    this.sessionslistTarget.classList.toggle("d-none")
+    this.reviewslistTarget.classList.toggle("d-none")
+  }
 
+  displayReviews(event) {
+    event.preventDefault()
+    this.sessionslinkTarget.classList.toggle("mute")
+    this.reviewslinkTarget.classList.toggle("mute")
+    this.sessionslistTarget.classList.toggle("d-none")
+    this.reviewslistTarget.classList.toggle("d-none")
+  }
 }
