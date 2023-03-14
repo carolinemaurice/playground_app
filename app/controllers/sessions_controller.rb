9f07f37bc97887146fc034b8bd84b6e22cc16fa6
eliot rafat offor <eliot.rafat.offor@gmail.com>
2023-03-14 12:32:28 +0100

    ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     @session = Session.find(params[:id])
-    @session.destroy
+    @session.destroy if @session.creator == current_user
     redirect_to mysessions_path, status: :see_other
   end
 
