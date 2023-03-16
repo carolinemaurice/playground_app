c222cc5a24e19c9f80e50a41de545d557e0a96d9
Laurie Segalowitch <lausegalo@gmail.com>
2023-03-16 14:49:15 +0100

    :sparkles: PWA

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  config.hosts << /\A[a-z\d-]+\.loca\.lt\z/
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
